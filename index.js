e4bd64ed9e4a781d83021d586a2a269970358bf6
Zin Hoang <hoang.mantue@gmail.com>
2022-07-15 21:05:53 +1200

    New look for My Identity-Value

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 /* Use forEach to return every second item in the array */
-const array = ['test', 'whatever', 'really', 'whynot']
+// const array = ['test', 'whatever', 'really', 'whynot']
 
-function everySecondItem(arr) {
-  let result = []
-  arr.forEach((item, i) => {
-    if (i % 2 !== 0) {
-      result.push(item)
-    }
-  })
-  return result
-}
-console.log(everySecondItem(array))
+// function everySecondItem(arr) {
+//   let result = []
+//   arr.forEach((item, i) => {
+//     if (i % 2 !== 0) {
+//       result.push(item)
+//     }
+//   })
+//   return result
+// }
+// console.log(everySecondItem(array))
+
+sum = (a, b) => a + b
+let string = `what "is" 'going' on?`
+let array = ['who', 'nah']
+
+console.log(sum(5, 19) + string)
+array.map((x) => console.log(x))
