6cac10a2ff7be8076c621a0dacdf1c21a5145b12
Zin Hoang <hoang.mantue@gmail.com>
2022-07-20 01:49:43 +1200

    new look for home page

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-/* Use forEach to return every second item in the array */
-// const array = ['test', 'whatever', 'really', 'whynot']
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
+// https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+document.addEventListener('DOMContentLoaded', start)
 
-// function everySecondItem(arr) {
-//   let result = []
-//   arr.forEach((item, i) => {
-//     if (i % 2 !== 0) {
-//       result.push(item)
-//     }
-//   })
-//   return result
-// }
-// console.log(everySecondItem(array))
+function start() {
+  bindEventListeners(document.getElementsByClassName('index-input')[0].children)
+}
 
-sum = (a, b) => a + b
-let string = `what "is" 'going' on?`
-let array = ['who', 'nah']
+function bindEventListeners() {
+  addEventListener('contextmenu', darkBackgrd)
+  addEventListener('click', yellowBackgrd)
+}
 
-console.log(sum(5, 19) + string)
-array.map((x) => console.log(x))
+function darkBackgrd(evt) {
+  evt.preventDefault()
+  document.body.style.backgroundColor = 'rgb(2, 9, 26)'
+}
+function yellowBackgrd() {
+  document.body.style.backgroundColor = '#dda200'
+}
