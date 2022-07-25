ac5b03e1b62e980923dfeddc85b351b1b32ce4de
Zin Hoang <hoang.mantue@gmail.com>
2022-07-25 23:10:08 +1200

    add reflection blog

--- index.js
+++ index.js
@@ -4,18 +4,22 @@
 document.addEventListener('DOMContentLoaded', start)
 
 function start() {
-  bindEventListeners(document.getElementsByClassName('index-input')[0].children)
+  document.getElementsByClassName('index-input')[0].onclick = function () {
+    changeBackgrd()
+  }
 }
 
-function bindEventListeners() {
-  addEventListener('contextmenu', darkBackgrd)
-  addEventListener('click', yellowBackgrd)
-}
-
-function darkBackgrd(evt) {
-  evt.preventDefault()
-  document.body.style.backgroundColor = 'rgb(2, 9, 26)'
-}
-function yellowBackgrd() {
-  document.body.style.backgroundColor = '#dda200'
+function changeBackgrd() {
+  let backgrdColor = document.body.style.backgroundColor
+  if (backgrdColor === 'rgb(2, 9, 26)') {
+    document.body.style.backgroundColor = 'rgb(12, 116, 107)'
+    document.getElementById('index-heading-3').style.textShadow = 'none'
+    document.getElementById('index-content').style.textShadow = 'none'
+  } else {
+    document.body.style.backgroundColor = 'rgb(2, 9, 26)'
+    document.getElementById('index-heading-3').style.textShadow =
+      '8px 2px 10px rgb(208, 227, 240)'
+    document.getElementById('index-content').style.textShadow =
+      '8px 2px 10px rgb(208, 227, 240)'
+  }
 }
