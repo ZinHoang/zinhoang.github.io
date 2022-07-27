1cff5c5cf9cadd333e6607e27c2f2ad3d355c40a
Zin Hoang <hoang.mantue@gmail.com>
2022-07-28 01:47:55 +1200

    update blogs

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls
-// the start function. More info:
-// https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start() {
-  document.getElementsByClassName('index-input')[0].onclick = function () {
-    changeBackgrd()
-  }
+  document.getElementsByClassName('index-input')[0].onclick = changeBackgrd
 }
 
 function changeBackgrd() {
-  let backgrdColor = document.body.style.backgroundColor
-  if (backgrdColor === 'rgb(2, 9, 26)') {
-    document.body.style.backgroundColor = 'rgb(12, 116, 107)'
-    document.getElementById('index-heading-3').style.textShadow = 'none'
-    document.getElementById('index-content').style.textShadow = 'none'
+  let indexHeader = document.getElementById('header-welcome').style
+  let indexHeading = document.getElementById('index-heading-3').style
+  let indexContent = document.getElementById('index-content').style
+
+  let backgrd = document.getElementById('index-background').style
+
+  if (
+    (backgrd.backgroundImage =
+      'url("../zinhoang.github.io/images/homepage-bckgrd1.webp")')
+  ) {
+    backgrd.backgroundImage =
+      'url("../zinhoang.github.io/images/homepage-bckgrd2.jpg")'
+    indexHeader.color = 'rgb(218, 67, 67)'
+    indexHeader.textShadow = 'none'
+    indexHeading.color = 'white'
+    indexContent.color = 'white'
   } else {
-    document.body.style.backgroundColor = 'rgb(2, 9, 26)'
-    document.getElementById('index-heading-3').style.textShadow =
-      '8px 2px 10px rgb(208, 227, 240)'
-    document.getElementById('index-content').style.textShadow =
-      '8px 2px 10px rgb(208, 227, 240)'
+    // backgrd.backgroundImage =
+    //   'url("../zinhoang.github.io/images/homepage-bckgrd1.webp")'
+    console.log('different background detected')
   }
 }
