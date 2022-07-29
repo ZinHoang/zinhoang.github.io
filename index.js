353bee08184386cdbaa7e67a025c56adef6968ae
Zin Hoang <hoang.mantue@gmail.com>
2022-07-29 19:04:18 +1200

    Update homepage

--- index.js
+++ index.js
@@ -4,25 +4,10 @@ function start() {
   document.getElementsByClassName('index-input')[0].onclick = changeBackgrd
 }
 
-function changeBackgrd() {
-  let indexHeader = document.getElementById('header-welcome').style
-  let backgrd = document.getElementById('index-background').style
+function changeBackgrd(evt) {
+  let body = document.getElementById('index-body')
+  let headerColor = document.getElementById('header-welcome')
 
-  if (
-    (backgrd.backgroundImage =
-      'url("../zinhoang.github.io/images/homepage-bckgrd1.webp")')
-  ) {
-    backgrd.backgroundImage =
-      'url("../zinhoang.github.io/images/homepage-bckgrd2.jpg")'
-    indexHeader.color = 'rgb(218, 67, 67)'
-    indexHeader.textShadow = 'none'
-    console.log('are you ok')
-  } else if (
-    (backgrd.backgroundImage =
-      'url("../zinhoang.github.io/images/homepage-bckgrd2.jpg")')
-  ) {
-    // backgrd.backgroundImage =
-    //   'url("../zinhoang.github.io/images/homepage-bckgrd1.webp")'
-    console.log('different background detected')
-  }
+  body.classList.toggle('index-background2')
+  headerColor.classList.toggle('header-welcome2')
 }
